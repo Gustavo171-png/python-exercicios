f9a3717aee330be9f7c8ac89428c077f6223e623
Gustavo171-png <gustavoda19.2019@gmail.com>
2026-04-09 23:08:37 -0300

    novos exercicios

---
+++ Aula 06.py
@@ -0,0 +1,3 @@
+produto = float(input('Digite o preço:'))
+desconto = float(5) 
+print ('O Valor com desconto é',produto - ( produto * desconto /100))
---
+++ Calculadora simples.py
@@ -0,0 +1,7 @@
+n = int(input('Digite um número:'))
+n1 = int(input('Digite outro número:'))
+print ('Adição:' ,n+n1)
+print ('Subtração:' ,n-n1)
+print ('Mutiplicação:', n*n1)
+print ('Divisão:', n/n1)
+    
